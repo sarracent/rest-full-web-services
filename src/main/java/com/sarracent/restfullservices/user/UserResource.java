393af220394f389d7c8677dd87037d806aaec08b
Damian Sarracent <damian.sarracent@sondeos.com.ar>
2020-07-30 01:09:38 -0300

    first commit

---
+++ src/main/java/com/sarracent/restfullservices/user/UserResource.java
@@ -0,0 +1,53 @@
+package com.sarracent.restfullservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    @Autowired
+    private UserDaoServices service;
+
+    @GetMapping("/users")
+    public List<User> retriveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retriveUser(@PathVariable int id) {
+        User user = service.findOne(id);
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+
+        return user;
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = service.save(user);
+
+        URI location = ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/id")
+            .buildAndExpand(savedUser.getId()).toUri();
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        User user = service.deleteById(id);
+        if(user == null)
+            throw new UserNotFoundException("id-" + id);
+
+    }
+
+}
