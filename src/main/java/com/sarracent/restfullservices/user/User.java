473e8ad887df968eec78d79b683db5d7996f3f42
Damian Sarracent <damian.sarracent@sondeos.com.ar>
2020-07-31 02:27:19 -0300

    Implementing Dynamic Filtering for RESTful Service

--- src/main/java/com/sarracent/restfullservices/user/User.java
+++ src/main/java/com/sarracent/restfullservices/user/User.java
@@ -1,18 +1,24 @@
 package com.sarracent.restfullservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user. ")
 public class User {
 
     private Integer id;
 
 
     @Size(min=2, message = "Name should have atleast 2 characters")
+    @ApiModelProperty(notes = "Name should have atleast 2 characters")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
 
     public User() {
